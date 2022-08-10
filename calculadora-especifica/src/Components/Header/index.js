f49a9a7aaa7be006e569197192bb26725fd9d967
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2022-08-10 10:50:31 -0300

    tornando layout responsivo

--- calculadora-especifica/src/Components/Header/index.js
+++ calculadora-especifica/src/Components/Header/index.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className='header'>
             <h1 className='header__title'>Calculadora Personalizada</h1>
-            <span> Teste técnico 01 - Escribo </span>
+            <span className='header__text'> Teste técnico 01 - Escribo </span>
         </header>
     )
 }
